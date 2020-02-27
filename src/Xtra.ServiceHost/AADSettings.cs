782f09aba0061b5c193e41b000f7ae181ffe01a9
Michael Monsour <michael@lethek.net>
2020-02-27 16:06:36 +1000

    Starting over again but using for .NET Core 3.0 Service-Workers

---
+++ src/Xtra.ServiceHost/AADSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace Xtra.ServiceHost
+{
+
+    public class AADSettings
+    {
+        public string ClientId { get; set; }
+        public string ClientSecret { get; set; }
+        public string CertThumbprint { get; set; }
+    }
+
+}
---
+++ src/Xtra.ServiceHost/Extensions/ConfigurationBuilderExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.Services.AppAuthentication;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.AzureKeyVault;
+
+using Xtra.ServiceHost.Helpers;
+
+
+namespace Xtra.ServiceHost.Extensions
+{
+
+    public static class ConfigurationBuilderExtensions
+    {
+        public static IConfigurationBuilder ConfigureAzureKeyVault(this IConfigurationBuilder self, string keyVault, AADSettings aadSettings)
+        {
+            if (String.IsNullOrEmpty(keyVault)) {
+                return self;
+            }
+
+            if (!String.IsNullOrEmpty(aadSettings.CertThumbprint)) {
+                var cert = CertHelper.TryFindCertificate(aadSettings.CertThumbprint);
+                self.AddAzureKeyVault(keyVault, aadSettings.ClientId, cert);
+            } else if (!String.IsNullOrEmpty(aadSettings.ClientSecret)) {
+                self.AddAzureKeyVault(keyVault, aadSettings.ClientId, aadSettings.ClientSecret);
+            } else {
+                var tokenProvider = new AzureServiceTokenProvider();
+                var tokenCallback = new KeyVaultClient.AuthenticationCallback(tokenProvider.KeyVaultTokenCallback);
+                var kvc = new KeyVaultClient(tokenCallback);
+                self.AddAzureKeyVault(keyVault, kvc, new DefaultKeyVaultSecretManager());
+            }
+
+            return self;
+        }
+    }
+
+}
