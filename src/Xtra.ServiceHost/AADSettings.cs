0cde3a5901fa4a36a0e5cac0761ccc1007c9d91f
Michael Monsour <michael@lethek.net>
2020-05-13 15:48:24 +1000

    Got rid of indirect dependency on old version of Microsoft.IdentityModel.Clients.ActiveDirectory and migrated to the current (preview) version of Azure.Extensions.Configuration.Secrets and Azure.Identity. Renamed the IConfigurationBuilder.ConfigureAzureKeyVault extension method to IConfigurationBuilder.AddAzureKeyVault. Added two new AADSettings properties: TenantId and Interactive. The AddAzureKeyVault method's keyVault parameter can now be either the name of a key-vault or an absolute key-vault URL.

--- src/Xtra.ServiceHost/AADSettings.cs
+++ src/Xtra.ServiceHost/AADSettings.cs
@@ -3,9 +3,11 @@
 
     public class AADSettings
     {
+        public string TenantId { get; set; }
         public string ClientId { get; set; }
         public string ClientSecret { get; set; }
         public string CertThumbprint { get; set; }
+        public bool Interactive { get; set; } = false;
     }
 
 }
