a671ed7fb93bfe826018dad770298b3e6923f34b
Michael Monsour <michael@lethek.net>
2022-05-30 16:28:05 +1000

    Relocated relevant code from Xtra.Common.Identity to new project here Xtra.ServiceHosting.Identity. Updated to use the FluentCertificates.Finder package.

---
+++ src/Xtra.ServiceHosting.Identity/AADCredential.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Azure.Core;
+using Azure.Identity;
+
+using FluentCertificates;
+
+using Xtra.Models.Settings;
+
+
+namespace Xtra.ServiceHosting.Identity;
+
+public class AADCredential : TokenCredential
+{
+    public AADCredential(AADSettings aadSettings)
+    {
+        var creds = new List<TokenCredential>();
+
+        if (aadSettings == null) {
+            aadSettings = new AADSettings();
+        }
+
+        if (aadSettings.Cert != null) {
+            creds.Add(new ClientCertificateCredential(aadSettings.TenantId, aadSettings.ClientId, aadSettings.Cert));
+        }
+
+        if (!String.IsNullOrEmpty(aadSettings.CertThumbprint)) {
+            var cert = CertificateFinder.Create()
+                .AddCommonStores()
+                .FirstOrDefault(x => x.Thumbprint.Equals(aadSettings.CertThumbprint, StringComparison.OrdinalIgnoreCase));
+            creds.Add(new ClientCertificateCredential(aadSettings.TenantId, aadSettings.ClientId, cert));
+        }
+
+        if (!String.IsNullOrEmpty(aadSettings.ClientSecret)) {
+            creds.Add(new ClientSecretCredential(aadSettings.TenantId, aadSettings.ClientId, aadSettings.ClientSecret));
+        }
+
+        creds.Add(new DefaultAzureCredential(aadSettings.Interactive));
+
+        _innerCredential = new ChainedTokenCredential(creds.ToArray());
+    }
+
+
+    public override ValueTask<AccessToken> GetTokenAsync(TokenRequestContext requestContext, CancellationToken cancellationToken)
+        => _innerCredential.GetTokenAsync(requestContext, cancellationToken);
+
+
+    public override AccessToken GetToken(TokenRequestContext requestContext, CancellationToken cancellationToken)
+        => _innerCredential.GetToken(requestContext, cancellationToken);
+
+
+    private readonly TokenCredential _innerCredential;
+}
