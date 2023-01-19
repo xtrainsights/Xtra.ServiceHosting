0aeaa17020656fb6dd0e85068b77f1e9beb52754
Michael Monsour <michael@lethek.net>
2023-01-19 10:15:55 +1000

    Added ServiceCollection extension methods for registering factories

--- src/Xtra.ServiceHosting/Extensions/ServiceCollectionExtensions.cs
+++ src/Xtra.ServiceHosting/Extensions/ServiceCollectionExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+
 using Microsoft.Extensions.DependencyInjection;
 
 using Xtra.ServiceHosting.DependencyInjection;
@@ -9,7 +10,25 @@ namespace Xtra.ServiceHosting.Extensions;
 
 public static class ServiceCollectionExtensions
 {
-    public static IServiceCollection AddServiceBundle<T>(this IServiceCollection services) where T : IServiceBundle, new()
+    public static void AddFactory<T>(this IServiceCollection services)
+        where T : class
+    {
+        services.AddTransient<T>();
+        services.AddSingleton<Func<T>>(x => x.GetService<T>);
+    }
+
+
+    public static void AddFactory<TService, TImplementation>(this IServiceCollection services)
+        where TService : class
+        where TImplementation : class, TService
+    {
+        services.AddTransient<TService, TImplementation>();
+        services.AddSingleton<Func<TService>>(x => x.GetService<TService>);
+    }
+
+
+    public static IServiceCollection AddServiceBundle<T>(this IServiceCollection services)
+        where T : IServiceBundle, new()
         => LoadBundle(services, Activator.CreateInstance<T>());
 
 
@@ -25,7 +44,8 @@ public static class ServiceCollectionExtensions
         => LoadBundles(services, bundles);
 
 
-    private static IServiceCollection LoadBundle<T>(IServiceCollection services, T bundle) where T : IServiceBundle
+    private static IServiceCollection LoadBundle<T>(IServiceCollection services, T bundle)
+        where T : IServiceBundle
     {
         bundle.Load(services);
         return services;
